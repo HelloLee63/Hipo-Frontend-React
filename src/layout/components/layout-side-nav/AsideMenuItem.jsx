dc3035a1c885ded6e29ad6b42df78ba7672d2bc2
HelloLee63 <82855877+HelloLee63@users.noreply.github.com>
2021-12-28 18:45:22 +0800

    Update css

--- src/layout/components/layout-side-nav/AsideMenuItem.jsx
+++ src/layout/components/layout-side-nav/AsideMenuItem.jsx
@@ -3,8 +3,6 @@ import { Link, useLocation } from 'react-router-dom'
 import { checkIsActive } from '../../../_metronic/helpers/RouterHelpers.ts'
 import { KTSVG } from '../../../_metronic/helpers/components/KTSVG.tsx'
 import clsx from 'clsx'
-import { useLayout } from '../../core'
-
 
 const AsideMenuItem = ({
     chiildren,
@@ -16,23 +14,33 @@ const AsideMenuItem = ({
   }) => {
       const {pathname} = useLocation()
       const isActive = checkIsActive(pathname, to)
-      const {config} = useLayout()
-      const {aside} = config
-
         return (
+            // <div className='menu-item'>
+            //     <Link className={clsx('menu-link without-sub', {active: isActive})} to={to}>
+            //         {hasBullet && (
+            //             <span className='menu-bullet'>
+            //                 <span className='bullet bullet-dot'></span>
+            //             </span>
+            //         )}
+            //         {icon && aside.menuIcon === 'svg' && (
+            //             <span className='menu-icon'>
+            //                 <KTSVG path={icon} className='svg-icon-2' />
+            //             </span>
+            //         )}
+            //         {fontIcon && aside.menuIcon === 'font' && <i className={clsx('bi fs-3', fontIcon)}></i>}
+            //         <span className='menu-title'>{title}</span>
+            //     </Link>
+            //     {chiildren}            
+            // </div>
+
             <div className='menu-item'>
-                <Link className={clsx('menu-link without-sub', {active: isActive})} to={to}>
-                    {hasBullet && (
-                        <span className='menu-bullet'>
-                            <span className='bullet bullet-dot'></span>
-                        </span>
-                    )}
-                    {icon && aside.menuIcon === 'svg' && (
-                        <span className='menu-icon'>
-                            <KTSVG path={icon} className='svg-icon-2' />
-                        </span>
-                    )}
-                    {fontIcon && aside.menuIcon === 'font' && <i className={clsx('bi fs-3', fontIcon)}></i>}
+                <Link className={clsx('menu-link without-sub', {active: isActive})} to={to}>                    
+                    {/* <span className='menu-bullet'>
+                        <span className='bullet bullet-dot'></span>
+                    </span>                    */}
+                    <span className='menu-icon'>
+                        <KTSVG path={icon} className='svg-icon-2' />
+                    </span>               
                     <span className='menu-title'>{title}</span>
                 </Link>
                 {chiildren}            
