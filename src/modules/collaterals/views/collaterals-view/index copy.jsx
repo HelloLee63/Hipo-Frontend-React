0a474644a9080129255ab1641f74246f5977246e
HelloLee63 <82855877+HelloLee63@users.noreply.github.com>
2022-03-09 18:53:57 +0800

    update

---
+++ src/modules/collaterals/views/collaterals-view/index copy.jsx
@@ -0,0 +1,221 @@
+import BigNumber from "bignumber.js"
+import { useCallback, useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+import TransactionLink from "../../../../components/button/transaction-link"
+import { useKnownTokens } from "../../../../components/providers/knownTokensProvider"
+import { usePools } from "../../../../components/providers/poolsProvider"
+import CollateralToken from "../../../../components/token-icon/CollateralToken"
+import TransactionAssetDataItem from "../../../../components/transaction-data-item/TransactionAssetDataItem"
+import TransactionCollateralDataItem from "../../../../components/transaction-data-item/TransactionCollateralDataItem"
+import TransactionLtvDataItem from "../../../../components/transaction-data-item/TransactionLtvDataItem"
+import WalletUnconnected from "../../../../components/wallet-unconnected"
+import { useWallet } from "../../../../wallets/walletProvider"
+import { useProtocolData } from "../../../../web3/components/providers/ProtocolDataProvider"
+import { useWalletData } from "../../../../web3/components/providers/WalletDataProvider"
+
+import { useColPool } from "../../../pledge/providers/colPool-provider"
+
+const CollateralsView = () => {
+
+  const walletCtx = useWallet()
+
+  const { collateralPools } = usePools()
+  const { setPoolSymbol } = useColPool()
+
+  const protocolData = useProtocolData()  
+  const walletData = useWalletData()
+
+  const { getTokenByAddress } = useKnownTokens()
+
+  const [isPledged, setIsPledged] = useState(false)
+  const [loading, setLoading] = useState(0)
+
+  // const getBalanceOfCollateral = useCallback(() => {
+    
+  //   let amount = new BigNumber(0)
+
+  //   if (walletCtx.account) {
+  //     collateralPools.map(pool => {
+  //       const balance = new BigNumber(pool.contract.balances?.get(walletCtx.account))
+  //       amount = BigNumber.sum(amount, balance)
+  //     })
+  //   }
+  //   return amount 
+
+  // }, [walletCtx.account, collateralPools])
+
+  function getBalanceOfCollateral() {
+
+    console.log('this is rendered');
+
+    setLoading(() => 5)
+    setLoading(() => 6)
+
+    console.log(loading);
+
+    if (walletCtx.account) {
+      let amount = new BigNumber(0)
+      collateralPools.map(async pool => {
+        console.log('await is executed');
+        const balance = await pool.contract.balances?.get(walletCtx.account)
+        amount = BigNumber.sum(amount, balance)
+      })
+
+      console.log('amount is ', amount);
+
+      if(amount?.gt(0)) {
+        setIsPledged(() => true)
+      }
+
+      if(amount?.eq(0) || amount?.isNaN()) {
+        setIsPledged(() => false)
+      }
+    }
+
+    setLoading(() => 6)
+     
+  }
+
+  useEffect(() => getBalanceOfCollateral(), [walletCtx.account])
+  // {
+  //   if(walletCtx.account) {
+
+  //     const balance = getBalanceOfCollateral()
+  //     if(balance?.gt(0)) {
+  //       setIsPledged(() => true)
+  //     }
+  
+  //     if(balance?.eq(0) || balance?.isNaN()) {
+  //       setIsPledged(() => false)
+  //     }
+  //   }
+  // }, [walletCtx.account])
+
+  console.log(loading);
+
+  if (walletCtx.account && !isPledged) {
+    return (
+      <>
+        { loading ? (
+        <div className='text-center pt-15 pb-15'>
+          <div className="spinner-border text-primary" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+        ) : (
+        // <div>No Pledged</div>
+
+        <div className='text-center pt-15 pb-15'>
+          <div className="spinner-border text-primary" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+        )}
+      </>
+      
+    )
+  }
+
+  if (!walletCtx.account) {
+    return (
+      <WalletUnconnected />
+    )
+  }
+
+  function handleCurrenSymbol(symbol) {
+    setPoolSymbol(() => symbol)
+  }
+
+  return (
+    <>
+      <TransactionLink name='Pledge' transaction='/pledge'/>
+      <div className='row g-5 g-xl-8'>
+    { 
+      collateralPools.map(pool =>  (
+        pool.contract.balances?.get(walletCtx.account)?.toString() > 0 &&
+        <div key={pool.collateralAsset.symbol} className='col-xl-4'>
+          <div className="card mb-2">
+            <div className="card-body pt-3 pb-3">
+              
+              <CollateralToken 
+                tokenIcon={pool.collateralAsset.icon} 
+                tokenSymbol={pool.collateralAsset.symbol} 
+                tokenName={pool.collateralAsset.name} 
+              />
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="card-body">
+
+              <TransactionCollateralDataItem
+                title='Amount'
+                tokenIcon={pool.collateralAsset.icon}
+                balance={pool.contract.balances?.get(walletCtx.account)}
+                decimals={pool.collateralAsset.decimals}
+              />
+
+              <div className='separator my-5'></div>
+
+              <TransactionAssetDataItem 
+                title='Debts'
+                tokenIcon={getTokenByAddress(walletData.getIssuerTotalDebts(pool.collateralAsset.address)?.debtAAddress?.toLowerCase())?.icon}
+                balance={walletData.getIssuerTotalDebts(pool.collateralAsset.address)?.debtAAmount}
+                decimals={getTokenByAddress(walletData.getIssuerTotalDebts(pool.collateralAsset.address)?.debtAAddress?.toLowerCase())?.decimals}
+              />
+
+              <TransactionAssetDataItem 
+                title=''
+                tokenIcon={getTokenByAddress(walletData.getIssuerTotalDebts(pool.collateralAsset.address)?.debtBAddress?.toLowerCase())?.icon}
+                balance={walletData.getIssuerTotalDebts(pool.collateralAsset.address)?.debtBAmount}
+                decimals={getTokenByAddress(walletData.getIssuerTotalDebts(pool.collateralAsset.address)?.debtBAddress?.toLowerCase())?.decimals}
+              />              
+
+              <div className='separator my-5'></div>
+
+              <TransactionLtvDataItem 
+                title='LTV'
+                balance={walletData.getIssuerLtv(pool.collateralAsset.address)?.ltv}
+                decimals={18}
+              />
+
+              <TransactionLtvDataItem 
+                title='Max LTV'
+                balance={protocolData.protocolDataContract.maxLtvMap?.get(pool.collateralAsset.address)}
+                decimals={2}
+              />
+
+              <TransactionLtvDataItem 
+                title='Liquidation Threshold'
+                balance={protocolData.protocolDataContract.thresholdMap.get(pool.collateralAsset.address)}
+                decimals={2}
+              />
+            </div>
+          </div>
+
+          <div className='d-flex flex-stack pt-3'>
+            <Link to='/redeem'>
+              <button onClick={() => handleCurrenSymbol(pool.collateralAsset.symbol)} className='btn btn-lg btn-primary me-0'>
+                <span className='indicator-label'>  
+                  Redeem                                   
+                </span>
+              </button>
+            </Link>
+
+            <Link to='/pledge'>
+              <button className='btn btn-lg btn-primary me-0'>
+                <span className='indicator-label'>              
+                  Add
+                </span>
+              </button>
+            </Link>
+          </div>       
+        </div>))            
+    }        
+      </div>
+    </>
+      
+  )
+}
+
+export default CollateralsView
--- src/modules/collaterals/views/collaterals-view/index.jsx
+++ src/modules/collaterals/views/collaterals-view/index.jsx
@@ -2,6 +2,7 @@ import BigNumber from "bignumber.js"
 import { useCallback, useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import TransactionLink from "../../../../components/button/transaction-link"
+import NoTransaction from "../../../../components/no-transaction"
 import { useKnownTokens } from "../../../../components/providers/knownTokensProvider"
 import { usePools } from "../../../../components/providers/poolsProvider"
 import CollateralToken from "../../../../components/token-icon/CollateralToken"
@@ -12,7 +13,6 @@ import WalletUnconnected from "../../../../components/wallet-unconnected"
 import { useWallet } from "../../../../wallets/walletProvider"
 import { useProtocolData } from "../../../../web3/components/providers/ProtocolDataProvider"
 import { useWalletData } from "../../../../web3/components/providers/WalletDataProvider"
-
 import { useColPool } from "../../../pledge/providers/colPool-provider"
 
 const CollateralsView = () => {
@@ -27,100 +27,81 @@ const CollateralsView = () => {
 
   const { getTokenByAddress } = useKnownTokens()
 
-  const [isPledged, setIsPledged] = useState(false)
-  const [loading, setLoading] = useState(0)
+  const [pledgedAmount, setPledgeAmount] = useState()
+  
+  const [conStatus, setConStatus] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [noTxsStatus, setNoTxsStatus] = useState(false)
+  const [txsStatus, setTxsStatus] = useState(false)
 
-  // const getBalanceOfCollateral = useCallback(() => {
+  const getBalanceOfCollateral = async () => {
     
-  //   let amount = new BigNumber(0)
-
-  //   if (walletCtx.account) {
-  //     collateralPools.map(pool => {
-  //       const balance = new BigNumber(pool.contract.balances?.get(walletCtx.account))
-  //       amount = BigNumber.sum(amount, balance)
-  //     })
-  //   }
-  //   return amount 
-
-  // }, [walletCtx.account, collateralPools])
-
-  function getBalanceOfCollateral() {
-
-    console.log('this is rendered');
-
-    setLoading(() => 5)
-    setLoading(() => 6)
-
-    console.log(loading);
+    let amount = new BigNumber(0)
 
     if (walletCtx.account) {
-      let amount = new BigNumber(0)
-      collateralPools.map(async pool => {
-        console.log('await is executed');
-        const balance = await pool.contract.balances?.get(walletCtx.account)
+      await collateralPools.map(async pool => {
+        const value = await pool.contract.balances?.get(walletCtx.account)
+        const balance = new BigNumber(value)
         amount = BigNumber.sum(amount, balance)
+        console.log(amount);
       })
 
-      console.log('amount is ', amount);
+      console.log(amount);
 
-      if(amount?.gt(0)) {
-        setIsPledged(() => true)
-      }
+      return amount
+    }    
+  }
 
-      if(amount?.eq(0) || amount?.isNaN()) {
-        setIsPledged(() => false)
-      }
+  useEffect(async () => {
+    if (walletCtx.account) {
+      const balance = await getBalanceOfCollateral()
+
+      console.log(balance);
+      setPledgeAmount(() => new BigNumber(balance))
+    }  
+  }, [walletCtx.account, 
+    collateralPools[0].contract.balances?.get(walletCtx.account),
+    collateralPools[1].contract.balances?.get(walletCtx.account),
+    collateralPools[2].contract.balances?.get(walletCtx.account)
+  ])
+
+  useEffect(() => {
+    if (!walletCtx.account) {
+      setConStatus(() => true)
+      setLoading(() => false)
+      setNoTxsStatus(() => false)
+      setTxsStatus(() => false)
+      return
     }
 
-    setLoading(() => 6)
-     
-  }
+    if (walletCtx.account) {
+      setConStatus(() => false)
 
-  useEffect(() => getBalanceOfCollateral(), [walletCtx.account])
-  // {
-  //   if(walletCtx.account) {
+      if (!pledgedAmount && !pledgedAmount?.eq(0)) {
+        setLoading(() => true)
 
-  //     const balance = getBalanceOfCollateral()
-  //     if(balance?.gt(0)) {
-  //       setIsPledged(() => true)
-  //     }
-  
-  //     if(balance?.eq(0) || balance?.isNaN()) {
-  //       setIsPledged(() => false)
-  //     }
-  //   }
-  // }, [walletCtx.account])
+        setNoTxsStatus(() => false)
+        setTxsStatus(() => false)
+        return
+      }
 
-  console.log(loading);
+      if (pledgedAmount?.gt(0)) {
+        setLoading(() => false)
 
-  if (walletCtx.account && !isPledged) {
-    return (
-      <>
-        { loading ? (
-        <div className='text-center pt-15 pb-15'>
-          <div className="spinner-border text-primary" role="status">
-            <span className="sr-only">Loading...</span>
-          </div>
-        </div>
-        ) : (
-        // <div>No Pledged</div>
+        setNoTxsStatus(() => false)
+        setTxsStatus(() => true)
+        return
+      }
 
-        <div className='text-center pt-15 pb-15'>
-          <div className="spinner-border text-primary" role="status">
-            <span className="sr-only">Loading...</span>
-          </div>
-        </div>
-        )}
-      </>
-      
-    )
-  }
+      if (pledgedAmount?.eq(0)) {
+        setLoading(() => false)
 
-  if (!walletCtx.account) {
-    return (
-      <WalletUnconnected />
-    )
-  }
+        setNoTxsStatus(() => true)
+        setTxsStatus(() => false)
+        return
+      }
+    }
+  }, [walletCtx.account, pledgedAmount])
 
   function handleCurrenSymbol(symbol) {
     setPoolSymbol(() => symbol)
@@ -128,93 +109,119 @@ const CollateralsView = () => {
 
   return (
     <>
-      <TransactionLink name='Pledge' transaction='/pledge'/>
-      <div className='row g-5 g-xl-8'>
-    { 
-      collateralPools.map(pool =>  (
-        pool.contract.balances?.get(walletCtx.account)?.toString() > 0 &&
-        <div key={pool.collateralAsset.symbol} className='col-xl-4'>
-          <div className="card mb-2">
-            <div className="card-body pt-3 pb-3">
-              
-              <CollateralToken 
-                tokenIcon={pool.collateralAsset.icon} 
-                tokenSymbol={pool.collateralAsset.symbol} 
-                tokenName={pool.collateralAsset.name} 
-              />
+      { loading &&
+        <>
+          <TransactionLink name='Pledge' transaction='/pledge'/>        
+          <div className=''>
+            <div className="spinner-border text-primary" role="status">
+              <span className="sr-only">Loading...</span>
             </div>
           </div>
+        </>
+      }
 
-          <div className="card">
-            <div className="card-body">
+      {
+        conStatus && <WalletUnconnected />
+      }
 
-              <TransactionCollateralDataItem
-                title='Amount'
-                tokenIcon={pool.collateralAsset.icon}
-                balance={pool.contract.balances?.get(walletCtx.account)}
-                decimals={pool.collateralAsset.decimals}
-              />
+      {
+        noTxsStatus && 
+        <>
+          <TransactionLink name='Pledge' transaction='/pledge'/>
+          <NoTransaction />
+        </>        
+      }
 
-              <div className='separator my-5'></div>
-
-              <TransactionAssetDataItem 
-                title='Debts'
-                tokenIcon={getTokenByAddress(walletData.getIssuerTotalDebts(pool.collateralAsset.address)?.debtAAddress?.toLowerCase())?.icon}
-                balance={walletData.getIssuerTotalDebts(pool.collateralAsset.address)?.debtAAmount}
-                decimals={getTokenByAddress(walletData.getIssuerTotalDebts(pool.collateralAsset.address)?.debtAAddress?.toLowerCase())?.decimals}
-              />
-
-              <TransactionAssetDataItem 
-                title=''
-                tokenIcon={getTokenByAddress(walletData.getIssuerTotalDebts(pool.collateralAsset.address)?.debtBAddress?.toLowerCase())?.icon}
-                balance={walletData.getIssuerTotalDebts(pool.collateralAsset.address)?.debtBAmount}
-                decimals={getTokenByAddress(walletData.getIssuerTotalDebts(pool.collateralAsset.address)?.debtBAddress?.toLowerCase())?.decimals}
-              />              
-
-              <div className='separator my-5'></div>
-
-              <TransactionLtvDataItem 
-                title='LTV'
-                balance={walletData.getIssuerLtv(pool.collateralAsset.address)?.ltv}
-                decimals={18}
-              />
-
-              <TransactionLtvDataItem 
-                title='Max LTV'
-                balance={protocolData.protocolDataContract.maxLtvMap?.get(pool.collateralAsset.address)}
-                decimals={2}
-              />
-
-              <TransactionLtvDataItem 
-                title='Liquidation Threshold'
-                balance={protocolData.protocolDataContract.thresholdMap.get(pool.collateralAsset.address)}
-                decimals={2}
-              />
-            </div>
+      { txsStatus && 
+        <>
+          <TransactionLink name='Pledge' transaction='/pledge'/>
+          <div className='row g-5 g-xl-8'>
+            { 
+              collateralPools.map(pool =>  (
+                pool.contract.balances?.get(walletCtx.account)?.toString() > 0 &&
+                <div key={pool.collateralAsset.symbol} className='col-xl-4'>
+                  <div className="card mb-2">
+                    <div className="card-body pt-3 pb-3">
+                      
+                      <CollateralToken 
+                        tokenIcon={pool.collateralAsset.icon} 
+                        tokenSymbol={pool.collateralAsset.symbol} 
+                        tokenName={pool.collateralAsset.name} 
+                      />
+                    </div>
+                  </div>
+    
+                  <div className="card">
+                    <div className="card-body">
+    
+                      <TransactionCollateralDataItem
+                        title='Amount'
+                        tokenIcon={pool.collateralAsset.icon}
+                        balance={pool.contract.balances?.get(walletCtx.account)}
+                        decimals={pool.collateralAsset.decimals}
+                      />
+    
+                      <div className='separator my-5'></div>
+    
+                      <TransactionAssetDataItem 
+                        title='Debts'
+                        tokenIcon={getTokenByAddress(walletData.getIssuerTotalDebts(pool.collateralAsset.address)?.debtAAddress?.toLowerCase())?.icon}
+                        balance={walletData.getIssuerTotalDebts(pool.collateralAsset.address)?.debtAAmount}
+                        decimals={getTokenByAddress(walletData.getIssuerTotalDebts(pool.collateralAsset.address)?.debtAAddress?.toLowerCase())?.decimals}
+                      />
+    
+                      <TransactionAssetDataItem 
+                        title=''
+                        tokenIcon={getTokenByAddress(walletData.getIssuerTotalDebts(pool.collateralAsset.address)?.debtBAddress?.toLowerCase())?.icon}
+                        balance={walletData.getIssuerTotalDebts(pool.collateralAsset.address)?.debtBAmount}
+                        decimals={getTokenByAddress(walletData.getIssuerTotalDebts(pool.collateralAsset.address)?.debtBAddress?.toLowerCase())?.decimals}
+                      />              
+    
+                      <div className='separator my-5'></div>
+    
+                      <TransactionLtvDataItem 
+                        title='LTV'
+                        balance={walletData.getIssuerLtv(pool.collateralAsset.address)?.ltv}
+                        decimals={18}
+                      />
+    
+                      <TransactionLtvDataItem 
+                        title='Max LTV'
+                        balance={protocolData.protocolDataContract.maxLtvMap?.get(pool.collateralAsset.address)}
+                        decimals={2}
+                      />
+    
+                      <TransactionLtvDataItem 
+                        title='Liquidation Threshold'
+                        balance={protocolData.protocolDataContract.thresholdMap.get(pool.collateralAsset.address)}
+                        decimals={2}
+                      />
+                    </div>
+                  </div>
+    
+                  <div className='d-flex flex-stack pt-3'>
+                    <Link to='/redeem'>
+                      <button onClick={() => handleCurrenSymbol(pool.collateralAsset.symbol)} className='btn btn-lg btn-primary me-0'>
+                        <span className='indicator-label'>  
+                          Redeem                                   
+                        </span>
+                      </button>
+                    </Link>
+    
+                    <Link to='/pledge'>
+                      <button className='btn btn-lg btn-primary me-0'>
+                        <span className='indicator-label'>              
+                          Add
+                        </span>
+                      </button>
+                    </Link>
+                  </div>       
+                </div>))            
+            }        
           </div>
-
-          <div className='d-flex flex-stack pt-3'>
-            <Link to='/redeem'>
-              <button onClick={() => handleCurrenSymbol(pool.collateralAsset.symbol)} className='btn btn-lg btn-primary me-0'>
-                <span className='indicator-label'>  
-                  Redeem                                   
-                </span>
-              </button>
-            </Link>
-
-            <Link to='/pledge'>
-              <button className='btn btn-lg btn-primary me-0'>
-                <span className='indicator-label'>              
-                  Add
-                </span>
-              </button>
-            </Link>
-          </div>       
-        </div>))            
-    }        
-      </div>
-    </>
-      
+        </>
+      }      
+    </>      
   )
 }
 
